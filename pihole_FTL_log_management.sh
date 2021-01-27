f0feda87848f0d8e017a0f7cda037996f8369b23
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2021-01-27 21:05:20 +0100

    Update pihole_FTL_log_management.sh

--- pihole_FTL_log_management.sh
+++ pihole_FTL_log_management.sh
@@ -3,7 +3,7 @@
 file=$([ -f /var/log/pihole-FTL.log.1 ] && echo "Found" || echo "Not Found")
 
 if [ "$file" = "Found" ];then
-	sudo gzip -9 /var/log/pihole-FTL.log.1 && sudo cp -p /var/log/pihole-FTL.log.1.gz /var/log/pihole/pihole-FTL.log.`date -d '-1 day' +"%Y-%m-%d"`.gz && sudo rm -f /var/log/pihole-FTL.log.1.gz
+	sudo gzip -9 /var/log/pihole-FTL.log.1 && sudo cp -p /var/log/pihole-FTL.log.1.gz /var/log/pihole/pihole-FTL.log.`date -d '-1 day' +"%Y-%m-%d"`.gz && sudo rm -f /var/log/pihole-FTL.log.1.gz || echo -e "Subject:Error during pihole FTL log rotation\n\nHello,\n\nThere was an error during pihole FTL log rotation." | /usr/sbin/sendmail yourmailexample@mail.com, yourmailexample2@mail.com
 
 else
         echo -e "Subject:The pihole-FTL.log.1 file was not found\n\nHello,\n\nThe /var/log/pihole-FTL.log.1 file was not found." | /usr/sbin/sendmail yourmailexample@mail.com, yourmailexample2@mail.com
