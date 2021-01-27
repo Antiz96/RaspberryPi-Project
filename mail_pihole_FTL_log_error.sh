e48b81b3a21fd79c2630c962665531a7a138969b
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2021-01-27 21:03:58 +0100

    Delete mail_pihole_FTL_log_error.sh

--- mail_pihole_FTL_log_error.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-error=$(cat /var/log/pihole/pihole_FTL_log_management_error.log | wc -l)
-
-if [ $error != 0 ];then
-	echo -e "Subject:Error in pihole-FTL.log management\n\nHello,\n\nError in /var/log/pihole_FTL_log_management_error.log." | /usr/sbin/sendmail yourmailexample@mail.com, yourmailexample2@mail.com
-fi
