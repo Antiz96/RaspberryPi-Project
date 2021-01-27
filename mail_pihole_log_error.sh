53cd78b0f79c464ed4b8aa7ae5f1d9f8d476737c
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2021-01-27 21:04:12 +0100

    Delete mail_pihole_log_error.sh

--- mail_pihole_log_error.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-error=$(cat /var/log/pihole/pihole_log_management_error.log | wc -l)
-
-if [ $error != 0 ];then
-	echo -e "Subject:Error in pihole.log management\n\nHello,\n\nError in /var/log/pihole_log_management_error.log." | /usr/sbin/sendmail yourmailexample@mail.com, yourmailexample2@mail.com
-fi
