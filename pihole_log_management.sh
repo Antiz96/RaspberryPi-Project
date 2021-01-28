09c5d7b42a50d7790c1b588fe78655f8bd8e218b
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2021-01-28 10:08:12 +0100

    Update pihole_log_management.sh

--- pihole_log_management.sh
+++ pihole_log_management.sh
@@ -3,7 +3,7 @@
 file=$([ -f /var/log/pihole.log.1 ] && echo "Found" || echo "Not Found")
 
 if [ "$file" = "Found" ];then
-	sudo gzip -9 /var/log/pihole.log.1 && sudo cp -p /var/log/pihole.log.1.gz /var/log/pihole/pihole.log.`date -d '-1 day' +"%Y-%m-%d"`.gz && sudo rm -f /var/log/pihole.log.1.gz || echo -e "Subject:Error during pihole FTL log rotation\n\nHello,\n\nThere was an error during pihole FTL log rotation." | /usr/sbin/sendmail yourmailexample@mail.com, yourmailexample2@mail.com
+	gzip -9 /var/log/pihole.log.1 && cp -p /var/log/pihole.log.1.gz /var/log/pihole/pihole.log.`date -d '-1 day' +"%Y-%m-%d"`.gz && rm -f /var/log/pihole.log.1.gz || echo -e "Subject:Error during pihole FTL log rotation\n\nHello,\n\nThere was an error during pihole FTL log rotation." | /usr/sbin/sendmail yourmailexample@mail.com, yourmailexample2@mail.com
 
 else 
 	echo -e "Subject:The pihole.log.1 file was not found\n\nHello,\n\nFile /var/log/pihole.log.1 was not found." | /usr/sbin/sendmail yourmailexample@mail.com, yourmailexample2@mail.com
